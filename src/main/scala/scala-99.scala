8e9a3ee0d1a8c669d0de83c08f542b8df912d5d2
Anatoly Yakovenko <aeyakovenko@gmail.com>
2016-04-12 16:47:53 -0700

    test3

--- src/main/scala/scala-99.scala
+++ src/main/scala/scala-99.scala
@@ -1,23 +1,20 @@
-// P01 (*) Find the last element of a list.
-//     Example:
-//     scala> last(List(1, 1, 2, 3, 5, 8))
-//     res0: Int = 8
-
-// The start of the definition of last should be
-//     def last[A](l: List[A]): A = ...
-// The `[A]` allows us to handle lists of any type.
 package scala99 {
-  object P01 {
-    // There are several ways to solve this problem.  If we use builtins, it's very
-    // easy.
-    def lastBuiltin[A](ls: List[A]): A = ls.last
-  
-    // The standard functional approach is to recurse down the list until we hit
-    // the end.  Scala's pattern matching makes this easy.
-    def lastRecursive[A](ls: List[A]): A = ls match {
+  object Scala99 {
+    def last[A](ls: List[A]): A = ls match {
       case h :: Nil  => h
-      case _ :: tail => lastRecursive(tail)
+      case _ :: tail => last(tail)
       case _         => throw new NoSuchElementException
+    }
+
+    def penultimate[A](ls: List[A]): A = ls match {
+      case h :: Nil       => throw new NoSuchElementException
+      case h :: _ :: Nil  => h
+      case _ :: tail      => penultimate(tail)
+      case _              => throw new NoSuchElementException
+    }
+    def nth[A](n:Int, ls: List[A]): A = (n < 0) match {
+      case true => throw new IndexOutOfBoundsException 
+      case _    => ls.drop(n).head
     }
   }
 }
--- src/test/scala/scala-99.scala
+++ src/test/scala/scala-99.scala
@@ -2,18 +2,48 @@ import org.scalacheck.Properties
 import org.scalacheck.Prop.forAll
 import scala99._
 
-object P01_test extends Properties("P01") {
+object Scala99Test extends Properties("scala99") {
+
+  def last[A](ls: List[A]): A = ls.last
   property("islast") = forAll { (a: List[Int]) =>
     a match {
       case Nil =>
        try {
-         P01.lastRecursive(a)
+         Scala99.last(a)
          false
        } catch {
          case ex: NoSuchElementException => true
          case ex: Throwable => false
        }
-      case _ => P01.lastBuiltin(a) == P01.lastRecursive(a)
+      case _ => Scala99.last(a) == last(a)
+    }
+  }
+  def penultimate[A](ls: List[A]): A = ls.apply(ls.length - 2)
+  property("penultimate") = forAll { (a: List[Int]) =>
+    (a.length <= 1) match {
+      case true =>
+       try {
+         Scala99.penultimate(a)
+         false
+       } catch {
+         case ex: NoSuchElementException => true
+         case ex: Throwable => false
+       }
+      case _ => Scala99.penultimate(a) == penultimate(a)
+    }
+  }
+  property("nth") = forAll { (n : Int, a: List[Int]) =>
+    (a.length <= n || n < 0) match {
+      case true => 
+       try {
+         Scala99.nth(n, a)
+         false
+       } catch {
+         case ex: NoSuchElementException => true
+         case ex: IndexOutOfBoundsException => true
+         case ex: Throwable => false
+       }
+      case _ => Scala99.nth(n, a) == a.apply(n)
     }
   }
 }
