619aa62d56cbc3052f852a523dd144d9ce4c7591
Anatoly Yakovenko <aeyakovenko@gmail.com>
2016-04-12 21:01:04 -0700

    cleanup

--- src/test/scala/scala-99.scala
+++ src/test/scala/scala-99.scala
@@ -1,46 +1,39 @@
 import org.scalacheck.Properties
 import org.scalacheck.Prop.forAll
+import scala.util.{Try,Success,Failure}
 import scala99._
 
 object Scala99Test extends Properties("scala99") {
-  property("islast") = forAll { (a: List[Int]) =>
-    a match {
-      case Nil =>
-       try {
-         Scala99.last(a)
-         false
-       } catch {
-         case ex: NoSuchElementException => true
-         case ex: Throwable => false
-       }
-      case _ => Scala99.last(a) == a.last
+  property("last") = forAll { (a: List[Int]) =>
+    val x = Try(Scala99.last(a))
+    val y = Try(a.last)
+    (x,y) match {
+      case (Success(x), Success(y)) => x == y
+      case (Failure(x:NoSuchElementException), Failure(y:NoSuchElementException)) => true
+      case _ => false
     }
   }
   property("penultimate") = forAll { (a: List[Int]) =>
-    (a.length <= 1) match {
-      case true =>
-       try {
-         Scala99.penultimate(a)
-         false
-       } catch {
-         case ex: NoSuchElementException => true
-         case ex: Throwable => false
-       }
-      case _ => Scala99.penultimate(a) == a.apply(a.length - 2)
+    val x = Try(Scala99.penultimate(a))
+    val y = Try(a.apply(a.length - 2))
+
+    (x,y) match {
+      case (Success(x), Success(y)) => x == y
+      case (Failure(x:NoSuchElementException), Failure(y:NoSuchElementException)) => true
+      case (Failure(x:NoSuchElementException), Failure(y:IndexOutOfBoundsException)) => true
+      case _ => false
     }
+
   }
   property("nth") = forAll { (n : Int, a: List[Int]) =>
-    (a.length <= n || n < 0) match {
-      case true => 
-       try {
-         Scala99.nth(n, a)
-         false
-       } catch {
-         case ex: NoSuchElementException => true
-         case ex: IndexOutOfBoundsException => true
-         case ex: Throwable => false
-       }
-      case _ => Scala99.nth(n, a) == a.apply(n)
+    val x = Try(Scala99.nth(n, a))
+    val y = Try(a.apply(n))
+    (x,y) match {
+      case (Success(x), Success(y)) => x == y
+      case (Failure(x:NoSuchElementException), Failure(y:NoSuchElementException)) => true
+      case (Failure(x:NoSuchElementException), Failure(y:IndexOutOfBoundsException)) => true
+      case (Failure(x:IndexOutOfBoundsException), Failure(y:IndexOutOfBoundsException)) => true
+      case _ => false
     }
   }
 }
